8e103b3a7abb2de2037fe04df9240fa653855332
DevJohanAdrian <jamaringarciabusiness@gmail.com>
2025-09-25 23:55:21 -0500

    feat: rxjs operator examples

--- src/app/examples/rxjs/operators/of_from/of_from.component.ts
+++ src/app/examples/rxjs/operators/of_from/of_from.component.ts
@@ -17,5 +17,6 @@ export class OfFromComponent {
     of(1, 2, 3).subscribe((value) => (this.ofValues += value + ' '));
 
     from([4, 5, 6]).subscribe((value) => (this.fromValues += value + ' '));
+      from([4, 5, 6]).subscribe((value) => (this.fromValues += value + ' '));
   }
 }
