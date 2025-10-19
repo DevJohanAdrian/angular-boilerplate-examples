00bf921718901165708adb28bdd677f40981cfe6
DevJohanAdrian <jamaringarciabusiness@gmail.com>
2025-10-18 19:45:50 -0500

    feat: range operatorrxjs

--- src/app/examples/rxjs/operators/range/range.component.ts
+++ src/app/examples/rxjs/operators/range/range.component.ts
@@ -1,14 +1,20 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { range } from 'rxjs';
 
 @Component({
   selector: 'app-range',
   imports: [],
   template: `<h3>Numero generados</h3>
-  <p></p>
+  <p>{{ numeros().join(', ')}}</p>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 // Mostrar una lista numerada del 1 al 5 generada reactivamente.
 export class RangeComponent { 
   numeros = signal<number[]>([]);
+
+  constructor (){
+
+    range(1,5).subscribe( val => { this.numeros.update(arr => [...arr, val]) } );
+  }
 }
